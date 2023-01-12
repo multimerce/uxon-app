212e8f960e1805cf1e957809f6fdc2830ecdc3fb
dmitriy-schuka <dmitriy.schuka@devit-team.com>
2023-01-12 21:36:55 +0200

    feat(client): create basic segments page

---
+++ web/frontend/src/pages/Segments/Segments.jsx
@@ -0,0 +1,13 @@
+import { Page, Layout, Heading } from "@shopify/polaris";
+
+export default function Segments() {
+    return (
+        <Page narrowWidth>
+            <Layout>
+                <Layout.Section>
+                    <Heading>Segments</Heading>
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
+};
