241ba973f0333bdec649a9136a914c4a21b16726
dmitriy-schuka <dmitriy.schuka@devit-team.com>
2023-01-12 21:35:02 +0200

    feat(client): create basic integration page

---
+++ web/frontend/src/pages/Integration/Integration.jsx
@@ -0,0 +1,13 @@
+import { Page, Layout, Heading } from "@shopify/polaris";
+
+export default function Integration() {
+    return (
+        <Page narrowWidth>
+            <Layout>
+                <Layout.Section>
+                    <Heading>Integration</Heading>
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
+};
