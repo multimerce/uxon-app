3a9a613f365ef98f240c6c3952fae4a4821f4780
dmitriy-schuka <dmitriy.schuka@devit-team.com>
2023-01-12 21:22:42 +0200

    feat(client): create analytics basic page

---
+++ web/frontend/src/pages/Analytics/Analytics.jsx
@@ -0,0 +1,13 @@
+import { Page, Layout, Heading } from "@shopify/polaris";
+
+export default function Analytics() {
+    return (
+        <Page narrowWidth>
+            <Layout>
+                <Layout.Section>
+                    <Heading>Analytics</Heading>
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
+};
