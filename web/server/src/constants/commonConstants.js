4a4f6489e45acd1d63f43c0cdaf8419fae54f2f3
dmitriy-schuka <dmitriy.schuka@devit-team.com>
2023-01-23 20:20:40 +0200

    feat(server): create segments list fetch controller

--- web/server/src/constants/commonConstants.js
+++ web/server/src/constants/commonConstants.js
@@ -1,8 +1,11 @@
 module.exports = {
     SUCCESS_MESSAGES: {
-        CONNECT_TO_DB: 'Connected to database'
+        CONNECT_TO_DB: 'Connected to database',
     },
     ERROR_MESSAGES: {
-        DB_CONNECTION: 'Database connection error'
+        DB_CONNECTION: 'Database connection error',
+        FETCH_FAILED: 'Fetch error',
+        CREATE_FAILED: 'Create error',
+        MISSING_DATA: 'Data for request are missing or incorrect',
     }
 };
