fb9efd2bc9d00e736145375d28ef684c9f92ff81
dmitriy-schuka <dmitriy.schuka@devit-team.com>
2023-01-23 11:41:20 +0200

    feat(client): edited form fields

--- web/frontend/src/components/common/FormTextField/FormTextField.jsx
+++ web/frontend/src/components/common/FormTextField/FormTextField.jsx
@@ -1,28 +1,9 @@
-import React, {useCallback, useState} from 'react';
+import React from 'react';
 import {TextField} from '@shopify/polaris';
 import classNames from 'classnames';
 import styles from './FormTextField.module.scss';
 
-const FormTextField = ({name, subtype, isLabelHidden, label, value, onChange, addConditions}) => {
-    const [conditionValue, setConditionValue] = useState();
-
-    const handleSelectConditionChange = useCallback(
-        (newValue, conditionName) => {
-            setConditionValue(newValue);
-            addConditions((prevState) => {
-                return {
-                    ...prevState,
-                    [conditionName]: newValue,
-                }
-            });
-        },
-        [setConditionValue, addConditions],
-    );
-
-    const handleChange = useCallback((data) => {
-        onChange(data);
-    }, [onChange]);
-
+const FormTextField = ({name, subtype, isLabelHidden, label, value, addConditions}) => {
     const classnames = classNames((subtype === 'number') && styles.TextFieldNumberType);
 
     return (
@@ -32,7 +13,7 @@ const FormTextField = ({name, subtype, isLabelHidden, label, value, onChange, ad
                 type={subtype}
                 labelHidden={isLabelHidden}
                 value={value}
-                onChange={(newValue) => handleSelectConditionChange(newValue, name)}
+                onChange={(newValue) => addConditions(newValue, name)}
                 autoComplete='off'
             />
         </div>
