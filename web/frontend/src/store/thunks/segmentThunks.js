f1ffbb60091fd645ea78158047bd04f3dc4dff8d
dmitriy-schuka <dmitriy.schuka@devit-team.com>
2023-01-26 18:36:09 +0200

    feat(client): add segments list redux thunks

--- web/frontend/src/store/thunks/segmentThunks.js
+++ web/frontend/src/store/thunks/segmentThunks.js
@@ -1,5 +1,7 @@
-import {fetchSegmentAction, createSegmentAction} from '../actions';
+import {fetchSegmentAction, createSegmentAction, updateSegmentAction} from '../actions';
 
 export const fetchSegment = (id) => async (dispatch) => dispatch(fetchSegmentAction(id));
 
 export const createSegment = (data) => async (dispatch) => dispatch(createSegmentAction(data));
+
+export const updateSegment = (id, data) => async (dispatch) => dispatch(updateSegmentAction(id, data))
--- web/frontend/src/store/thunks/segmentsListThunks.js
+++ web/frontend/src/store/thunks/segmentsListThunks.js
@@ -1,3 +1,9 @@
-import {fetchSegmentsAction} from '../actions';
+import {fetchSegmentsAction, deleteSegmentAction, changeSegmentStatusAction, duplicateSegmentAction} from '../actions';
 
 export const fetchSegments = (fetchParams) => async (dispatch) => dispatch(fetchSegmentsAction(fetchParams));
+
+export const deleteSegment = (id) => async (dispatch) => dispatch(deleteSegmentAction(id));
+
+export const changeSegmentStatus = (id, newStatus) => async (dispatch) => dispatch(changeSegmentStatusAction(id, newStatus));
+
+export const duplicateSegment = (id) => async (dispatch) => dispatch(duplicateSegmentAction(id));
